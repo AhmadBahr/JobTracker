204407a9be85146c6be38cf4d9571a6a1a595d76
Ahmad <ahmadb.ahr@outlook.com>
2026-02-14 12:43:48 +0200

    Refactor JobApplicationRepository by removing unnecessary annotations and methods, simplifying the interface to extend JpaRepository directly.

--- backend/src/main/java/com/jobtracker/repository/JobApplicationRepository.java
+++ backend/src/main/java/com/jobtracker/repository/JobApplicationRepository.java
@@ -2,12 +2,5 @@ package com.jobtracker.repository;
 
 import com.jobtracker.entity.JobApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
-public interface JobApplicationRepository extends JpaRepository<JobApplication, String> {
-
-  List<JobApplication> findAllByOrderByAppliedDateDesc();
-}
+public interface JobApplicationRepository extends JpaRepository<JobApplication, String> {}
